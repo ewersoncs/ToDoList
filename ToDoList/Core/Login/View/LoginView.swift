c65d8b318caac89d5fa32934cdb1ea8301cca5d1
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2024-09-21 14:26:34 -0400

    chore: create navigation view to LoginView Page and MainView config

--- ToDoList/Core/Login/View/LoginView.swift
+++ ToDoList/Core/Login/View/LoginView.swift
@@ -7,20 +7,52 @@
 
 import SwiftUI
 
-// MARK: - PAREI EM 26:24 Youtube
+// MARK: - PAREI EM 33:47 Youtube
 
 struct LoginView: View {
+    
+    // MARK: - PROPERTIES
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        VStack{
-            // MARK: - HEADER
-            HeaderView()
-            
-            // MARK: - LOGIN FORM
-            
-            
-            // MARK: - CREATE ACCOUNT
-            
-            Spacer()
+        NavigationStack{
+            VStack{
+                // MARK: - HEADER
+                HeaderView()
+                
+                // MARK: - LOGIN FORM
+                Form{
+                    TextField("Endereço de Email", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    SecureField("Senha de Acesso", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        // TODO
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            
+                            Text("Acessar")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                }
+                
+                // MARK: - CREATE ACCOUNT
+                VStack {
+                    Text("Novo por aqui ?")
+      
+                    NavigationLink("Criar uma conta", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
         }
     }
 }
